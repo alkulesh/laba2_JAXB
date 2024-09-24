53ce5401cc2cc2528bdd9856bd42e1fc93f6f2fa
alkulesh <a.kulesh@internet.ru>
2024-09-24 23:37:58 +0300

    2

--- src/main/java/jaxb/model/Department.java
+++ src/main/java/jaxb/model/Department.java
@@ -7,7 +7,7 @@ package jaxb.model;
  * Time: 19:42
  */
 import java.util.List;
-
+//
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
